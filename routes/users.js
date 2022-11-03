55214eca5515344e3d75cd6d41fc56b680206ce8
Sayed Ahmed <sayeed205@gmail.com>
2022-11-03 15:49:40 +0530

    Add swagger ui - add docs route - /api/docs

--- routes/users.js
+++ routes/users.js
@@ -10,13 +10,226 @@ import { Router } from "express";
 
 const router = Router();
 
+// swagger ui schema
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    users:
+ *      type: object
+ *      required:
+ *        - firstName
+ *        - lastName
+ *        - age
+ *        - country
+ *      properties:
+ *        firstName:
+ *          type: string
+ *          description: First name of the user
+ *        lastName:
+ *          type: string
+ *          description: Last name of the user
+ *        age:
+ *          type: number
+ *          description: Age of the user
+ *        country:
+ *          type: string
+ *          description: Country name of the user
+ *      example:
+ *        firstName: Salman
+ *        lastName: Khan
+ *        age: 69
+ *        country: India
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: users
+ *  description: Manage users in the database
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *  get:
+ *    tags: [users]
+ *    summary: Returns all the users
+ *    responses:
+ *      200:
+ *        description: The list of the users
+ *        content:
+ *          application/json:
+ *            schema:
+ *              type: array
+ *              items:
+ *                $ref: '#/components/schemas/users'
+ *
+ */
+
 router.get("/", getAllUsers);
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *  get:
+ *    summary: Get a single user by id
+ *    tags: [users]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The user ID
+ *    responses:
+ *      200:
+ *        descriptions: The user details by user ID
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/users'
+ *      404:
+ *        description: user wasn't found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              users:
+ *                type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  description: Error message
+ *              example:
+ *                error: User doesn't exist
+ */
 
 router.get("/:id", getSingleUser);
 
+/**
+ * @swagger
+ * /api/users:
+ * post:
+ *    summary: Create a new user
+ *    tags: [users]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/users'
+ *    responses:
+ *      200:
+ *        description: The user is successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/users'
+ *      400:
+ *        description: Some server error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              users:
+ *                type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  description: Error message
+ */
+
+/**
+ * @swagger
+ * /api/users:
+ *  post:
+ *    summary: Create a new user
+ *    tags: [users]
+ *    requestBody:
+ *      required: true
+ *      description: Enter user description
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/users'
+ *    responses:
+ *      200:
+ *        description: The user is successfully created
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/users'
+ *      400:
+ *        description: Some server error
+ *        content:
+ *          application/json:
+ *            schema:
+ *              users:
+ *                type: object
+ *              properties:
+ *                error:
+ *                  type: string
+ *                  description: Error message
+ *
+ */
+
 router.post("/", createUser);
 
+/**
+ * @swagger
+ * /api/users/{id}:
+ *    delete:
+ *      summary: Remove a user from database by id
+ *      tags: [users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The user ID
+ *      responses:
+ *        200:
+ *          description: The user details is updated
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/users'
+ *        404:
+ *          description: The user not found
+ */
+
 router.delete("/:id", deleteSingleUser);
+
+/**
+ * @swagger
+ * /api/users/{id}:
+ *    patch:
+ *      summary: :Update and existing user
+ *      tags: [users]
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          schema:
+ *            type: string
+ *          required: true
+ *          description: The user ID
+ *      requestBody:
+ *        required: true
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/users'
+ *      responses:
+ *        200:
+ *          description: The user details is updated
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#//components/schemas/users'
+ *        404:
+ *          description: The user not found
+ *
+ */
 
 router.patch("/:id", updateSingleWorkout);
 
--- server.js
+++ server.js
@@ -2,12 +2,46 @@ import * as dotenv from "dotenv";
 
 import express from "express";
 import mongoose from "mongoose";
+import swaggerJSDoc from "swagger-jsdoc";
+import swaggerUI from "swagger-ui-express";
 import usersRoute from "./routes/users.js"; // import routes
 
 dotenv.config();
 
+const localServerURL = process.env.LOCAL_SERVER_URL;
+const cloudServerURL = process.env.CLOUD_SERVER_URL;
+
+// swaggerJsDoc options
+const options = {
+  definition: {
+    openapi: "3.0.3",
+    info: {
+      title: "Animo Internship Demo API",
+      version: "1.0.0",
+      description: "Demo API for Internship assignment in Animo",
+    },
+    servers: [
+      {
+        url: cloudServerURL,
+        description: "Cloud server or Production server",
+      },
+      {
+        url: localServerURL,
+        description: "Local dev server",
+      },
+    ],
+  },
+
+  apis: ["./routes/*.js"],
+};
+
+const specs = swaggerJSDoc(options);
+
 // express app
 const app = express();
+
+// swagger docs route
+app.use("/api/docs", swaggerUI.serve, swaggerUI.setup(specs));
 
 // middleware
 app.use(express.json());
